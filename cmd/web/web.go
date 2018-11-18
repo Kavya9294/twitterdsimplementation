4e288323ab850522fe32669c72d1f524ec313fc6
Nikhila Dwarakanath <nd1556@nyu.edu>
2018-11-18 12:41:28 -0500

    Removed Gorilla Mux

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -9,7 +9,6 @@ import (
 
 	"../../web/auth"
 	"../../web/auth/storage/memory"
-	"github.com/gorilla/mux"
 	//"os"
 	//"strings"
 )
@@ -22,27 +21,30 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 		log.Print("New users     -> ", mymem.Users)
 
 	}
-	w.WriteHeader(200)
+	//w.WriteHeader(200)
 
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 	t := template.Must(template.New("login").ParseFiles("../views/login.html"))
+
 	if r.Method == "POST" {
 		r.ParseForm()
 		log.Printf("Entering Login Handler POST %d", len(mymem.Users))
 		log.Print("Header ->")
 		log.Print(r.Header)
 		auth.DoAuthLogin(w, r)
-
+		//r.Method = "GET"
+		http.Redirect(w, r, "/posts", http.StatusFound)
 	}
-
-	err := t.ExecuteTemplate(w, "login.html", mymem.Users)
-	if err != nil {
-		log.Fatal("Some error: ", err)
+	if r.Method == "GET" {
+		err := t.ExecuteTemplate(w, "login.html", mymem.Users)
+		if err != nil {
+			log.Fatal("Some error: ", err)
+		}
 	}
-	w.WriteHeader(200)
+	//w.WriteHeader(200)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,20 +54,21 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	users := mymem.Users
 	cur_user := users[0]
 	cur_posts := cur_user.GetPosts(all_posts)
-
-	if r.Method == "POST" {
-		r.ParseForm()
-		newPost := mymem.Post{r.FormValue("author"), r.FormValue("desc")}
-		all_posts = newPost.AppendPost()
-		log.Print("postList in POST: ", all_posts)
-		http.Redirect(w, r, "/posts", http.StatusSeeOther)
-	}
-	log.Print("Posts for current user: ", cur_posts)
 	t := template.Must(template.New("posts").ParseFiles("../views/post.html"))
 	err := t.ExecuteTemplate(w, "post.html", cur_posts)
 	if err != nil {
 		log.Fatal("Some error: ", err)
 	}
+	if r.Method == "POST" {
+
+		r.ParseForm()
+		newPost := mymem.Post{r.FormValue("author"), r.FormValue("desc")}
+		all_posts = newPost.AppendPost()
+		log.Print("postList in POST: ", all_posts)
+		http.Redirect(w, r, "/posts", http.StatusSeeOther)
+
+	}
+	log.Print("Posts for current user: ", cur_posts)
 
 }
 
@@ -73,11 +76,9 @@ func main() {
 	mymem.Initialize()
 	log.Print("Calling init")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/signup", SignupHandler)
-	r.HandleFunc("/login", LoginHandler)
-	r.HandleFunc("/posts", PostHandler)
-	http.Handle("/", r)
+	http.HandleFunc("/signup", SignupHandler)
+	http.HandleFunc("/login", LoginHandler)
+	http.HandleFunc("/posts", PostHandler)
 
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
